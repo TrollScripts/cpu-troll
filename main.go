7cd0d391bd9b1b6d4cfc6a0981b0a98d0924784b
Evan Burrell <burrelle@oregonstate.edu>
2018-12-19 15:06:44 +0000

    Refactor to be able to unit test

--- main.go
+++ main.go
@@ -8,25 +8,34 @@ import (
 	"time"
 )
 
-func main() {
-
-	args := os.Args[1:]
-
+func readArguments(args []string) (int64, int64) {
 	percentageLoad, err := strconv.ParseInt(args[0], 10, 64)
 	if err != nil {
 		fmt.Println("Invalid percentage load")
+		percentageLoad = 0
 	}
-
 	secondsToRun, err := strconv.ParseInt(args[1], 10, 64)
 	if err != nil {
 		fmt.Println("Invalid seconds to run")
+		secondsToRun = 0
 	}
+	return percentageLoad, secondsToRun
+}
 
-	var percentage float64 = float64((float64(percentageLoad) / 100.0))
-	var cpuCount float64 = float64(runtime.NumCPU()) * percentage
+func calcuatePercentage(percentageLoad int64) float64 {
+	return float64((float64(percentageLoad) / 100.0))
+}
+
+func calcuateCPUCount(percentage float64, CPU int) float64 {
+	return float64(CPU) * percentage
+}
+
+func outputLoadAndCount(cpuCount, percentage float64, percentageLoad int64) {
 	fmt.Println(fmt.Sprintf("%v * ( %v / %v ) = %v", runtime.NumCPU(), percentageLoad, 100, cpuCount))
 	fmt.Println(fmt.Sprintf("%v * %v = %v", runtime.NumCPU(), percentage, cpuCount))
+}
 
+func troll(cpuCount float64, secondsToRun int64) {
 	done := make(chan int)
 
 	for i := 0; i < int(cpuCount); i++ {
@@ -43,4 +52,13 @@ func main() {
 
 	time.Sleep(time.Second * time.Duration(secondsToRun))
 	close(done)
+}
+
+func main() {
+	args := os.Args[1:]
+	var percentageLoad, secondsToRun = readArguments(args)
+	var percentage = calcuatePercentage(percentageLoad)
+	var cpuCount = calcuateCPUCount(percentage, runtime.NumCPU())
+	outputLoadAndCount(cpuCount, percentage, percentageLoad)
+	troll(cpuCount, secondsToRun)
 }
